65673a9d1fa1eab925df0afe24b30edd08f8e20c
AbdelRahman Mahmoud <ARahman.Mahmoud@linkdev.com>
2017-08-13 15:03:21 +0200

    added base classes for inline forms

---
+++ angular/src/shared/form-component-base.ts
@@ -0,0 +1,40 @@
+﻿import { Injector, ElementRef, AfterViewInit, ViewChildren, QueryList, ViewChild, EventEmitter, Output, Input } from '@angular/core';
+import { ModalDirective } from 'ngx-bootstrap';
+import { AppComponentBase } from '@shared/app-component-base';
+
+export abstract class FormComponentBase<EntityDto> extends AppComponentBase implements AfterViewInit {
+
+    elementRef: ElementRef;
+    @Output() modalSave: EventEmitter<any> = new EventEmitter<any>();
+
+    @ViewChild('modalContent') modalContent: ElementRef;
+    @ViewChild('FormModal') modal: ModalDirective;
+    @ViewChildren('options') options: QueryList<any>;
+
+    item: EntityDto = null;
+    active: boolean = false;
+    saving: boolean = false;
+    isNew: boolean = true;
+
+    constructor(injector: Injector) {
+        super(injector);
+    }
+
+    ngAfterViewInit() {
+        this.options.changes.subscribe(() => {
+            (<any>$('select')).selectpicker('refresh');
+        });
+    }
+
+    onShown(): void {
+        $.AdminBSB.input.activate($(this.modalContent.nativeElement));
+        $('form').find('input[type=text],textarea,select').filter(':visible:first').focus();
+    }
+
+    close(): void {
+        this.active = false;
+        this.modal.hide();
+    }
+
+    
+}
---
+++ angular/src/shared/inline-form-component-base.ts
@@ -0,0 +1,43 @@
+﻿import { Injector, ElementRef, AfterViewInit, ViewChildren, QueryList,ViewChild, EventEmitter, Output, Input } from '@angular/core';
+import { ModalDirective } from 'ngx-bootstrap';
+import { FormComponentBase } from '@shared/form-component-base';
+export abstract class InlineFormComponentBase<EntityDto> extends FormComponentBase<EntityDto> implements AfterViewInit {
+
+    elementRef: ElementRef;
+    @ViewChild('modalContent') modalContent: ElementRef;
+    @ViewChild('FormModal') modal: ModalDirective;
+    @Output() modalSave: EventEmitter<any> = new EventEmitter<any>();
+
+    @ViewChildren('options')
+    options: QueryList<any>;
+
+
+    item: EntityDto = null;
+    active: boolean = false;
+    saving: boolean = false;
+    isNew: boolean = true;
+
+    @Output() saveItem: EventEmitter<EntityDto> = new EventEmitter<EntityDto>();
+    @Input() isChild?: boolean = false;
+    constructor(injector: Injector) {
+        super(injector);
+    }
+
+    ngAfterViewInit() {
+        this.options.changes.subscribe(() => {
+            (<any>$('select')).selectpicker('refresh');
+        });
+    }
+
+    bindData(data: EntityDto): void {
+        this.item = data;
+        this.active = true;
+        this.modal.show();
+    }
+
+    close(): void {
+        this.active = false;
+        this.modal.hide();
+    }
+
+}
