bc6357f74588ca6a9d1d1532add2fbdb46585628
Mariam.ElHawy <Mariam.ElHawy@MARIAM-ELHAWY>
2017-09-06 13:35:30 +0200

    Rana Changes

--- aspnet-core/src/AbpCompanyName.AbpProjectName.EntityFrameworkCore/Migrations/20170815154534_Adding StudentParentId foreignkey.Designer.cs
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.EntityFrameworkCore/Migrations/20170815154534_Adding StudentParentId foreignkey.Designer.cs
@@ -11,9 +11,10 @@ using Abp.Notifications;
 namespace AbpCompanyName.AbpProjectName.Migrations
 {
     [DbContext(typeof(AbpProjectNameDbContext))]
-    partial class AbpProjectNameDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170815154534_Adding StudentParentId foreignkey")]
+    partial class AddingStudentParentIdforeignkey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
